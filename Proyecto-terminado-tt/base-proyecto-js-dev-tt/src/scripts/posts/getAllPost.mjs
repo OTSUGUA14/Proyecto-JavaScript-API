f0a1db246e6191bbc3639a216710708acd8483f4
Augusto <otsuguariquelme@gmail.com>
2024-06-14 01:04:24 -0300

    like :(

--- Proyecto-terminado-tt/base-proyecto-js-dev-tt/src/scripts/posts/getAllPost.mjs
+++ Proyecto-terminado-tt/base-proyecto-js-dev-tt/src/scripts/posts/getAllPost.mjs
@@ -23,7 +23,7 @@ const comments =async (id,ver) => {
            
 
             comments.forEach(co => {
-                console.log(comments.length);
+
                 var newFrom = document.createElement("p");
                 newFrom.innerHTML=`@${co.user.username}: `
                 newFrom.classList.add('username');
@@ -52,12 +52,26 @@ const comments =async (id,ver) => {
     }
    
 }
+    const likess=(id,valor)=>{
 
+        var lii = document.querySelector(`#like${id}`);
+        
+        let valorActual = parseInt(lii.textContent, 10);
+        if (valor){
+            // Obtiene el valor actual del contador y lo convierte a número
+            valorActual++;
+        
+        }else{
+            valorActual--;
+        }
+        lii.textContent=valorActual;
+    
+    }
 
 
 export const getAllPost = async () => {
   document.querySelector('#posts').innerHTML = "";
-
+  
   try {
       const res = await fetch('https://dummyjson.com/posts?limit=9');
       const { posts } = await res.json();
@@ -67,7 +81,8 @@ export const getAllPost = async () => {
          
           const res = await fetch(`https://dummyjson.com/users/filter?key=id&value=${post.userId}`);
           const { users } = await res.json();
-          
+          const re = await fetch(`https://dummyjson.com/comments/post/${post.id}`)
+            const {comments} = await re.json()
           return `
               <div class="card2">
                   <div class="body">
@@ -81,33 +96,19 @@ export const getAllPost = async () => {
                                       <g id="SVGRepo_iconCarrier">
                                           <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" d="M16 10H16.01M12 10H12.01M8 10H8.01M3 10C3 4.64706 5.11765 3 12 3C18.8824 3 21 4.64706 21 10C21 15.3529 18.8824 17 12 17C11.6592 17 11.3301 16.996 11.0124 16.9876L7 21V16.4939C4.0328 15.6692 3 13.7383 3 10Z"></path>
                                       </g>
-                                  </svg>18
+                                  </svg>${comments.length}
                               </div>
                               <div>
-                                  <svg fill="#000000" xmlns:xlink="http://www.w3.org/2000/xlink" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="-2.5 0 32 32">
-                                      <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
-                                      <g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g>
-                                      <g id="SVGRepo_iconCarrier">
-                                          <g id="icomoon-ignore"></g>
-                                          <path fill="#000000" d="M0 10.284l0.505 0.36c0.089 0.064 0.92 0.621 2.604 0.621 0.27 0 0.55-0.015 0.836-0.044 3.752 4.346 6.411 7.472 7.060 8.299-1.227 2.735-1.42 5.808-0.537 8.686l0.256 0.834 7.63-7.631 8.309 8.309 0.742-0.742-8.309-8.309 7.631-7.631-0.834-0.255c-2.829-0.868-5.986-0.672-8.686 0.537-0.825-0.648-3.942-3.3-8.28-7.044 0.11-0.669 0.23-2.183-0.575-3.441l-0.352-0.549-8.001 8.001zM1.729 10.039l6.032-6.033c0.385 1.122 0.090 2.319 0.086 2.334l-0.080 0.314 0.245 0.214c7.409 6.398 8.631 7.39 8.992 7.546l-0.002 0.006 0.195 0.058 0.185-0.087c2.257-1.079 4.903-1.378 7.343-0.836l-13.482 13.481c-0.55-2.47-0.262-5.045 0.837-7.342l0.104-0.218-0.098-0.221-0.031 0.013c-0.322-0.632-1.831-2.38-7.498-8.944l-0.185-0.215-0.282 0.038c-0.338 0.045-0.668 0.069-0.981 0.069-0.595 0-1.053-0.083-1.38-0.176z"></path>
-                                      </g>
-                                  </svg>7
+                                  <label class="container lik"  data-id=${post.id}>
+    '                                             <input type="checkbox">
+                                        <svg id="Glyph" version="1.1" viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M29.845,17.099l-2.489,8.725C26.989,27.105,25.804,28,24.473,28H11c-0.553,0-1-0.448-1-1V13  c0-0.215,0.069-0.425,0.198-0.597l5.392-7.24C16.188,4.414,17.05,4,17.974,4C19.643,4,21,5.357,21,7.026V12h5.002  c1.265,0,2.427,0.579,3.188,1.589C29.954,14.601,30.192,15.88,29.845,17.099z" id="XMLID_254_"></path><path d="M7,12H3c-0.553,0-1,0.448-1,1v14c0,0.552,0.447,1,1,1h4c0.553,0,1-0.448,1-1V13C8,12.448,7.553,12,7,12z   M5,25.5c-0.828,0-1.5-0.672-1.5-1.5c0-0.828,0.672-1.5,1.5-1.5c0.828,0,1.5,0.672,1.5,1.5C6.5,24.828,5.828,25.5,5,25.5z" id="XMLID_256_"></path></svg>
+                                        </label><p id="like${post.id}">${post.reactions.likes}'</p>
                               </div>
                               <div>
-                                  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                                      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                                      <g id="SVGRepo_iconCarrier">
-                                          <path opacity="0.1" d="M21 6C21 7.65685 19.6569 9 18 9C16.3431 9 15 7.65685 15 6C15 4.34315 16.3431 3 18 3C19.6569 3 21 4.34315 21 6Z" fill="#323232"></path>
-                                          <path opacity="0.1" d="M21 18C21 19.6569 19.6569 21 18 21C16.3431 21 15 19.6569 15 18C15 16.3431 16.3431 15 18 15C19.6569 15 21 16.3431 21 18Z" fill="#323232"></path>
-                                          <path opacity="0.1" d="M9 12C9 13.6569 7.65685 15 6 15C4.34315 15 3 13.6569 3 12C3 10.3431 4.34315 9 6 9C7.65685 9 9 10.3431 9 12Z" fill="#323232"></path>
-                                          <path d="M21 6C21 7.65685 19.6569 9 18 9C16.3431 9 15 7.65685 15 6C15 4.34315 16.3431 3 18 3C19.6569 3 21 4.34315 21 6Z" stroke-width="2"></path>
-                                          <path d="M21 18C21 19.6569 19.6569 21 18 21C16.3431 21 15 19.6569 15 18C15 16.3431 16.3431 15 18 15C19.6569 15 21 16.3431 21 18Z" stroke-width="2"></path>
-                                          <path d="M9 12C9 13.6569 7.65685 15 6 15C4.34315 15 3 13.6569 3 12C3 10.3431 4.34315 9 6 9C7.65685 9 9 10.3431 9 12Z" stroke-width="2"></path>
-                                          <path d="M8.7207 10.6397L15.0001 7.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                          <path d="M8.70605 13.353L15 16.5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                      </g>
-                                  </svg>2
+                                <label class="container dis" data-id=${post.id}>
+                                    <input type="checkbox">
+                                    <svg id="Glyph" version="1.1" viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M2.156,14.901l2.489-8.725C5.012,4.895,6.197,4,7.528,4h13.473C21.554,4,22,4.448,22,5v14  c0,0.215-0.068,0.425-0.197,0.597l-5.392,7.24C15.813,27.586,14.951,28,14.027,28c-1.669,0-3.026-1.357-3.026-3.026V20H5.999  c-1.265,0-2.427-0.579-3.188-1.589C2.047,17.399,1.809,16.12,2.156,14.901z" id="XMLID_259_"></path><path d="M25.001,20h4C29.554,20,30,19.552,30,19V5c0-0.552-0.446-1-0.999-1h-4c-0.553,0-1,0.448-1,1v14  C24.001,19.552,24.448,20,25.001,20z M27.001,6.5c0.828,0,1.5,0.672,1.5,1.5c0,0.828-0.672,1.5-1.5,1.5c-0.828,0-1.5-0.672-1.5-1.5  C25.501,7.172,26.173,6.5,27.001,6.5z" id="XMLID_260_"></path></svg>
+                                    </label><p id="dislike${post.id}">${post.reactions.dislikes}'</p>
                               </div>
                           </div>
                           <div class="viewer">
@@ -165,27 +166,38 @@ export const getAllPost = async () => {
 
   
       const coment = document.querySelectorAll('.co');
-        
-        coment.forEach(coo => {
-            var ver=true
+    
+      const dislike=document.querySelectorAll('.dis')
+   
+    coment.forEach(coo => {
+        var ver=true
           coo.addEventListener('click', () => {
             if (ver){
-               
                 comments(coo.dataset.id,ver)
                 ver=false
-                console.log(ver);
+                
             }else{
-
                 //optener el valor del id
                 comments(coo.dataset.id,ver)
-                    ver=true
-                    console.log(ver);
+                    ver=true 
             }
-                
-            
           });
       });
+      const like=document.querySelectorAll('.lik')
+    like.forEach(li =>{
+       
+       let isLiked = false;
 
+        li.addEventListener('click', (e) => {
+            e.stopPropagation();
+            isLiked = !isLiked;
+            console.log(`Button ID: ${li.dataset.id}, isLiked: ${isLiked}`);
+          likess(li.dataset.id,isLiked)
+      });
+      });
+      
+//   dislike.addEventListener('click', () => {
+//   });
   } catch (e) {
       console.error(e);
   }
